1e0bcb6015edf61815e44888cee9b690a731ba82
Krish Makwana <krishmakwanaapop0801@gmail.com>
2026-06-15 17:22:34 -0700

    Add chat fn and ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Imports
+import json
 import os
 import requests
 import gradio as gr
@@ -24,7 +25,6 @@ model_image = "gpt-image-1"
 # System message to guide assistant's behavior
 system_prompt = """
 You are skycast, a helpful assistant that gives live weather data given a city using an API. 
-You also generate audio for the messages you return, and can generate images of the city as well.
 If you do not know the answer, just say so. 
 """
 
@@ -51,7 +51,7 @@ def get_weather(city):
 
 # Description of get_weather function
 weather_function = {
-    "name": get_weather,
+    "name": "get_weather",
     "description": "Get the current weather for a given city.",
     "parameters": {
         "type": "object",
@@ -68,7 +68,7 @@ weather_function = {
 
 
 # tools list
-tools = [{"type": "function", "function": "weather_function"}]
+tools = [{"type": "function", "function": weather_function}]
 
 
 # artist(): Generates an image for the given city
@@ -95,23 +95,87 @@ def talker(message):
         return f.name
 
 
-# handle_tool_calls(): Handles calls to the get_weather function
-def handle_tool_calls():
-    return 0
+# handle_tool_calls_and_cities(): Handles calls to the get_weather function, and extracts cities
+def handle_tool_calls(message):
+    responses = []
+    cities = []
+
+    for tool_call in message.tool_calls:
+        if tool_call.function.name == "get_weather":
+            arguments = json.loads(tool_call.function.arguments)
+            city = arguments.get("city")
+            cities.append(city)
+            weather_details = get_weather(city)
+            responses.append(
+                {
+                    "role":"tool",
+                    "content":weather_details,
+                    "tool_call_id" : tool_call.id
+                }
+            )
+    return responses, cities
 
 # chat(): Chat function for gradio, maintaining history, performing LLM responses, and calling tools if necessary
-def chat(message, history):
-    return 0
+def chat(history):
+    history = [{"role": h["role"], "content" : h["content"]} for h in history]
+    messages = [{"role": "system", "content": system_prompt}] + history
+
+    response = openai.chat.completions.create(
+        model = model_main,
+        messages = messages,
+        tools = tools
+    )
+    cities = []
+    image = None
+
+    while response.choices[0].finish_reason == "tool_calls":
+        message = response.choices[0].message
+        responses, cities = handle_tool_calls(message)
+        messages.append(message)
+        messages.extend(responses)
+        response = openai.chat.completions.create(
+            model = model_main,
+            messages = messages,
+            tools = tools
+        )
+
+    reply = response.choices[0].message.content
+    history+=[{"role": "assistant", "content": reply}]
+    voice = talker(reply)
+    if cities:
+        image = artist(cities[0])
+    return history, voice, image
+
+# put_message_in_chatbox(): Helper function to add user messages to the chat history
+def put_message_in_chatbox(message, history):
+    return "", history + [{"role": "user", "content": message}]
 
 # build_ui: Generates the gradio UI for the assistant
 def build_ui():
-    return 0
+    with gr.Blocks() as ui:
+        with gr.Row():
+            chatbot = gr.Chatbot(height = 500)
+            image_output = gr.Image(height = 500, interactive = False)
+        with gr.Row():
+            audio_output = gr.Audio(interactive = False, autoplay = True)
+
+        with gr.Row():
+            message = gr.Textbox("Chat with skycast!!!")
+
+        message.submit(
+                        put_message_in_chatbox,
+                        inputs = [message, chatbot],
+                        outputs = [message, chatbot]
+                ).then(
+                        chat,
+                        inputs = [chatbot],
+                         outputs = [chatbot, audio_output, image_output]
+                    )
+    return ui
 
 def main():
-    print("Hello from skycast-ai!")
-    print(get_weather("New York"))
-    image = artist("New York")
-    image.save("test.png")
+    ui = build_ui()
+    ui.launch()
 
 
 
